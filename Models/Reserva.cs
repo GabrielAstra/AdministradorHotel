fb163b91fde5080f4de86634dfc2eee674151dba
GabrielAstra <gvlc2013@gmail.com>
2023-07-14 22:54:48 -0300

    AdmHotel

---
+++ Models/Reserva.cs
@@ -0,0 +1,53 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Atenção: A quantidade de hospedes é maior que a capacidade da suite!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = 0.1m;
+                decimal valorDesconto = valor * desconto;
+                valor -= valorDesconto;
+                Console.WriteLine($"Você recebeu um desconto de 10% por ultrapassar ou igualar a 10 dias de hospedagem.");
+            }
+
+            return valor;
+        }
+    }
+}
